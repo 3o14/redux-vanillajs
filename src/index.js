ba8f513f9c7417703b3caaeaeb4c7c9c0adc36c8
3o14 <dldnjswn4265@gmail.com>
2023-03-10 09:26:16 +0900

    주석 추가

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import { createStore } from "redux";
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
+
+// 상수 선언 for 실수 방지
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
+
 
 const reducer = (state = [], action) => {
   console.log(action);
@@ -17,10 +20,13 @@ const reducer = (state = [], action) => {
   }
 };
 
+// 상태(변화하는 데이터) 저장 공간 store
 const store = createStore(reducer);
 
+// subscribe(구독) : store의 변화를 감지할 때마다 발동되는 함수
 store.subscribe(() => console.log(store.getState()));
 
+// 입력된 할 일 표시란에 추가하기 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
